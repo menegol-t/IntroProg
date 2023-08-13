1046d0e5573cf0d9c56ed53ac4ddad6481a18a4e
Tomas Menegol <tomas.menegol@gmail.com>
2023-08-12 23:25:14 -0300

    recap of act

---
+++ practica1.py
@@ -0,0 +1,19 @@
+#act 6
+print("mi primer programa python")
+#act7
+print("v\ne\nr\nt\ni\nc\na\nl")
+#act8
+print("*****\n*   *\n*   *\n*   *\n*****")
+#act9
+print("*\n***\n*****\n*******\n*********")
+#act10
+print(input("Tu nombre es: "))
+#act11
+x = input("Ingresa un valor")
+print(f"********************\n* El resultado es: {x} \n********************")
+
+#Empieza act 12
+x =  10
+y = 5
+#act 12) 2) 1)
+print(x/y + 1)
