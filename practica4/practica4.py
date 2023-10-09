4ab6614a93ab1189e41aa0be19bf1c4799a8266f
Tomas Menegol <tomas.menegol@gmail.com>
2023-10-09 19:57:48 -0300

    practica5

---
+++ practica4/practica4.py
@@ -0,0 +1,11 @@
+print("\n actividad 3)A)")
+def repe (cadena):
+    print(cadena)
+    print(cadena)
+    print(cadena)
+    
+print("\n actividad 4)A)")
+def promedio2 (a, b):
+    return (a + b)/2
+def promedio3 (a, b, c):
+    return (a + b + c)/3
---
+++ practica4/practica4_llama3-4.py
@@ -0,0 +1,13 @@
+import practica4
+import math
+# print("\n actividad 1) A)B)C)D)")
+# x = float(input("Ingresa un numero decimal: "))
+# print(math.sqrt(x), abs(x), abs(x-3), math.sqrt(abs(x-5)))
+
+# a = input("Ingresa una cadena: ")
+x = float(input("Ingresa un numero decimal: "))
+y = float(input("Ingresa un numero decimal: "))
+z = float(input("Ingresa un numero decimal: "))
+# practica4.repe(a)
+print(practica4.promedio2(x, y))
+print(practica4.promedio3(x, y, z))
